f089df5aba66e814fa0da89b2f0fdd6d6f220199
Ross Paterson <ross@soi.city.ac.uk>
2006-11-08 14:26:02 +0000

    undef XDestroyImage instead of defining XUTIL_DEFINE_FUNCTIONS
    Comment in the file:
    Xutil.h overrides some functions with macros.
    In recent versions of X this can be turned off with
    
        #define XUTIL_DEFINE_FUNCTIONS
    
    before the #include, but this doesn't work with older versions.
    As a workaround, we undef the macros here.  Note that this is only
    safe for functions with return type int.

--- include/HsXlib.h
+++ include/HsXlib.h
@@ -13,8 +13,19 @@
 #include <X11/X.h>
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
-#define XUTIL_DEFINE_FUNCTIONS
 #include <X11/Xutil.h>
+
+/* Xutil.h overrides some functions with macros.
+ * In recent versions of X this can be turned off with
+ *
+ *	#define XUTIL_DEFINE_FUNCTIONS
+ *
+ * before the #include, but this doesn't work with older versions.
+ * As a workaround, we undef the macros here.  Note that this is only
+ * safe for functions with return type int.
+ */
+#undef XDestroyImage
+#undef AddPixel
 
 #define XK_MISCELLANY
 #define XK_LATIN1
